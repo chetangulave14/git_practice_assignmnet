8237e6945397af464a6b557b71a0881ddd4b7e90
chetan <chetangulave14@gmail.com>
2022-10-23 19:07:53 +0530

    check prime

---
+++ prime.js
@@ -0,0 +1,21 @@
+//Check whether a number is Prime or not
+
+var count = 0;
+for(var i=1;i<=num;i++){
+if(num%i==0){
+    count++;
+}
+}
+if(count==1){
+    console.log("Yes");
+}else {console.log("No");
+}
+
+//Check whether a string is palindrome or not
+
+var bag ="";
+var sum = 0;
+for(var i = 0;i<N;i++){
+ sum =str[i]+"";
+}
+  console.log(sum);
